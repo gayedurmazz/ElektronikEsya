ebd8d9cdbc57d57e556ff3d53e53b8c845da028f
muratcanabay <muratcanabay@hotmail.com>
2019-12-05 16:45:40 +0300

    Laptop ve Masaüstü Bilgisayar Sınıfları Eklendi.

--- src/main/java/com/uniyaz/Bilgisayar.java
+++ src/main/java/com/uniyaz/Bilgisayar.java
@@ -1,4 +1,13 @@
 package com.uniyaz;
 
-public class Bilgisayar {
+public abstract class Bilgisayar extends ElektronikEsya {
+    @Override
+    public void TusaBasCihazAc() {
+        System.out.println("Bilgisayar Açıldı.");
+    }
+
+    @Override
+    public void TusaBasCihazKapa() {
+        System.out.println("Bilgisayar Kapandı.");
+    }
 }
--- src/main/java/com/uniyaz/LaptopBilgisayar.java
+++ src/main/java/com/uniyaz/LaptopBilgisayar.java
@@ -1,4 +1,6 @@
 package com.uniyaz;
 
 public class LaptopBilgisayar extends Bilgisayar{
+    private int sarjSuresi; // Saat
+    private int ramBoyutu;
 }
